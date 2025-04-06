22240503d5ba6ba7be950317e946091222fd028b
Glazunova Tomara <glazunovatomara5@gmail.com>
2025-04-06 23:21:39 +0700

    feat:add html+css

---
+++ assets/script/script.js
@@ -0,0 +1,37 @@
+
+const result = document.getElementById('result')
+const category = document.getElementById('category')
+const num = document.getElementById('num')
+const err = document.getElementById('err')
+const lnk = 'https://swapi.py4e.com/api/'
+
+const addResult = () => {
+    categoryValue = category.value
+    numValue = num.value 
+
+    if(!numValue) {
+        err.textContent ='выберите номер'
+    }
+//    try {
+        fetch(`${lnk}${categoryValue}/${numValue}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+        //console.log(data);
+        console.log("1")
+            if(categoryValue !== 'films') {
+                result.textContent = data.name
+                console.log("2")
+            } else {
+                result.textContent = data.title
+                console.log("3")
+            } 
+        })
+//    } 
+    .catch ((error) => {
+        err.textContent = "Ошибка:"+ error
+        console.log("Ошибка:" + error)
+    })
+}
+//addResult()
